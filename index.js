fd3e1d6b141471eacf23a1f1d262904b9e38bcd7
Samuel Espin Tolosa <samuel.espintolosa@gmail.com>
2023-03-20 16:51:17 +0100

    Add: Welcome hook message

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 import { serve } from "https://deno.land/std@0.140.0/http/server.ts";
 
-
-const page = () => "Hello";
+const page = () => "What do you wish more?";
 
 serve((_req) => {
   return new Response(page(), {
