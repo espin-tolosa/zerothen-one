3778aeb79def114a9ee723431b76e6c6da9b5407
Samuel Espin Tolosa <samuel.espintolosa@gmail.com>
2023-03-25 19:32:44 +0100

    Add: Markdown

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import { serve } from "https://deno.land/std@0.140.0/http/server.ts";
 
-const page = () => "What do you wish more?";
+import { Marked } from '@ts-stack/markdown';
+
+const page = () => Marked.parse('What do you __wish__ more?');
 
 serve((_req) => {
   return new Response(page(), {
