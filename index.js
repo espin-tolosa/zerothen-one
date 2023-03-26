d5f2b1cc7bb4d7aa418e75dec2470ea7d79078cf
Samuel Espin Tolosa <samuel.espintolosa@gmail.com>
2023-03-26 22:05:15 +0200

    Add: cursor blink

--- index.js
+++ index.js
@@ -1,11 +1,71 @@
 import { serve } from "https://deno.land/std@0.140.0/http/server.ts";
 
-import { Marked } from '@ts-stack/markdown';
+//import { Marked } from '@ts-stack/markdown';
 
-const page = () => Marked.parse('What do you __wish__ more?');
+//const page = () => Marked.parse('What do you __wish__ more?');
+
+const page = () => `
+<!DOCTYPE html>
+<html>
+<head>
+<title>Title of the document</title>
+</head>
+
+<body>
+
+
+<div class="container">
+  <span class="box">
+    <h2>What do you wish more?</h2>
+  </span>
+</div>
+
+
+</body>
+
+</html>
+
+<style>
+.container {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+}
+
+.box {
+  padding: 0 50px 0 10px;
+  border: 2px solid #656464;
+  border-radius: 10px;
+}
+
+.box h2 {
+  display: flex;
+  align-items: center;
+  gap: 2px;
+}
+
+@keyframes cursor-blink {
+  0% {
+    opacity: 0;
+  }
+}
+
+.box h2::after {
+  content: "";
+  width: 5px;
+  height: 20px;
+  background: #ec7fff;
+  display: inline-block;
+  animation: cursor-blink 1.5s steps(2) infinite;
+}
+</style>
+
+`
+
 
 serve((_req) => {
   return new Response(page(), {
-    headers: { "content-type": "text/plain" },
+    headers: { "content-type": "html" },
   });
 });
