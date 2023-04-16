5d14f3929429975e4b4a0d266189d3bedd7bf230
Samuel Espin Tolosa <samuel.espintolosa@gmail.com>
2023-04-16 14:34:55 +0200

    Add: ppt

--- index.js
+++ index.js
@@ -4,6 +4,65 @@ import { serve } from "https://deno.land/std@0.140.0/http/server.ts";
 
 //const page = () => Marked.parse('What do you __wish__ more?');
 
+const page2 = () => `
+<!DOCTYPE html>
+<html>
+<head>
+  <title>My PowerPoint Slide</title>
+  <style>
+    body {
+      background-color: #1c1c1c;
+      color: #ffffff;
+      font-family: Arial, sans-serif;
+      margin: 0;
+      padding: 0;
+    }
+
+    .container {
+      margin: 0 auto;
+      max-width: 960px;
+      padding: 100px;
+      text-align: center;
+    }
+
+    h1 {
+      font-size: 72px;
+      margin-bottom: 50px;
+    }
+
+    p {
+      font-size: 36px;
+      line-height: 1.5;
+      margin-bottom: 50px;
+    }
+
+    .button {
+      background-color: #ffffff;
+      border-radius: 5px;
+      color: #1c1c1c;
+      display: inline-block;
+      font-size: 24px;
+      margin-top: 50px;
+      padding: 10px 20px;
+      text-decoration: none;
+      transition: background-color 0.3s ease;
+    }
+
+    .button:hover {
+      background-color: #f0f0f0;
+    }
+  </style>
+</head>
+<body>
+  <div class="container">
+    <h1>Welcome to My PowerPoint Slide</h1>
+    <p>This is an example of a web page that looks like a PowerPoint slide.</p>
+    <a href="#" class="button">Click Here</a>
+  </div>
+</body>
+</html>
+`
+
 const page = () => `
 <!DOCTYPE html>
 <html>
@@ -68,7 +127,7 @@ const page = () => `
 
 
 serve((_req) => {
-  return new Response(page(), {
+  return new Response(page2(), {
     headers: { "content-type": "html" },
   });
 });
